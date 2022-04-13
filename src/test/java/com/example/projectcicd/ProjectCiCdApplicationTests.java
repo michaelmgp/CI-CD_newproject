5ca287228837a4713b515e69a1c1790f1d05c79f
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-04-13 17:30:20 +0700

    code

---
+++ src/test/java/com/example/projectcicd/ProjectCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
